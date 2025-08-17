fa777fad8185e99774296806e8f77fc79ebdc449
Anna-Sta-Siia <arabynina11@yahoo.com>
2025-08-17 14:01:13 +0200

    Maj vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-// vite.config.js (simple et suffisant)
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+// ✅ équivalent ESM de __dirname
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => ({
   plugins: [svgr(), react()],
-  base: mode === "production" ? "/mon_projet/" : "/", // <-- adapter au nom du repo
+  base: mode === "production" ? "/mon_projet/" : "/",
+  build: {
+    rollupOptions: {
+      input: { main: resolve(__dirname, "index.html") },
+    },
+  },
 }));
